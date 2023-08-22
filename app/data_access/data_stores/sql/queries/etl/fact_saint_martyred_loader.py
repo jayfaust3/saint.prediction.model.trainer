ae49e40ba562b573118815b7d0d9f606c29b4441
Jay Faust <jay.faust3@gmail.com>
2023-08-21 22:54:19 -0400

    Added schedule and etl query

---
+++ app/data_access/data_stores/sql/queries/etl/fact_saint_martyred_loader.py
@@ -0,0 +1,22 @@
+FACT_SAINT_MARTYRED_LOADER_QUERY: str = '''
+TRUNCATE TABLE fact_saint_martyred;
+
+INSERT INTO fact_saint_martyred (year_of_death, age_at_death, region, martyred)
+SELECT
+	ranked_by_time.year_of_death,
+	ranked_by_time.year_of_death - ranked_by_time.year_of_birth age_at_death,
+	ranked_by_time.region,
+	ranked_by_time.martyred
+FROM
+(SELECT
+	id,
+	active,
+	year_of_birth,
+	year_of_death,
+	region,
+	martyred,
+	row_number() over(partition by id order by modified_date desc)
+FROM
+	saint_lake) ranked_by_time
+WHERE ranked_by_time.active = true AND ranked_by_time.row_number = 1;
+'''
--- cdk/stacks/lambda-stack.ts
+++ cdk/stacks/lambda-stack.ts
@@ -1,5 +1,7 @@
 import { App, Stack, StackProps } from 'aws-cdk-lib';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Rule, Schedule } from 'aws-cdk-lib/aws-events';
+import { LambdaFunction } from 'aws-cdk-lib/aws-events-targets';
 
 export class LambdaStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
@@ -12,6 +14,10 @@ export class LambdaStack extends Stack {
       environment: {}
     });
 
-    // const instanceActionPolicy = 
+    const eventRule = new Rule(this, `${sainyMartyredModelTrainerFn.functionName}`, {
+        schedule: Schedule.expression('cron(*/15 * * * *)')
+    });
+
+    eventRule.addTarget(new LambdaFunction(sainyMartyredModelTrainerFn))
   }
 }
