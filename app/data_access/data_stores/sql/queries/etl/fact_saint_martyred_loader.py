f36a4620133e8c8b502564faccdacac9faf7089a
Jay Faust <jay.faust3@gmail.com>
2023-08-22 22:15:23 -0400

    Added loader query to training

--- app/data_access/data_stores/sql/queries/etl/fact_saint_martyred_loader.py
+++ app/data_access/data_stores/sql/queries/etl/fact_saint_martyred_loader.py
@@ -1,22 +1,22 @@
 FACT_SAINT_MARTYRED_LOADER_QUERY: str = '''
-TRUNCATE TABLE {saint_martyred_fact_table_name};
+    TRUNCATE TABLE {saint_martyred_fact_table_name};
 
-INSERT INTO {saint_martyred_fact_table_name} (year_of_death, age_at_death, region, martyred)
-SELECT
-	ranked_by_time.year_of_death,
-	ranked_by_time.year_of_death - ranked_by_time.year_of_birth age_at_death,
-	ranked_by_time.region,
-	ranked_by_time.martyred
-FROM
-(SELECT
-	id,
-	active,
-	year_of_birth,
-	year_of_death,
-	region,
-	martyred,
-	row_number() over(partition by id order by modified_date desc)
-FROM
-	{saint_lake_table_name}) ranked_by_time
-WHERE ranked_by_time.active = true AND ranked_by_time.row_number = 1;
+    INSERT INTO {saint_martyred_fact_table_name} (year_of_death, age_at_death, region, martyred)
+    SELECT
+        ranked_by_time.year_of_death,
+        ranked_by_time.year_of_death - ranked_by_time.year_of_birth age_at_death,
+        ranked_by_time.region,
+        ranked_by_time.martyred
+    FROM
+    (SELECT
+        id,
+        active,
+        year_of_birth,
+        year_of_death,
+        region,
+        martyred,
+        row_number() over(partition by id order by modified_date desc)
+    FROM
+        {saint_lake_table_name}) ranked_by_time
+    WHERE ranked_by_time.active = true AND ranked_by_time.row_number = 1;
 '''
--- app/data_access/data_stores/sql/resolvers/query/ml_model_data.py
+++ app/data_access/data_stores/sql/resolvers/query/ml_model_data.py
@@ -1,5 +1,6 @@
 from typing import Union
 from os import getenv
+from ...queries.etl.fact_saint_martyred_loader import *
 from ...queries.ml_model_data.saint import *
 from ......core.enums.prediction_type import PredictionType
 
@@ -10,6 +11,10 @@ class MLModelDataQueryResolver(object):
     def resolve_query(prediction_type: PredictionType) -> Union[str, None]:
         match prediction_type:
             case PredictionType.MARTYRED:
-                return MARTYRED_QUERY.format(saint_martyred_fact_table_name=getenv('SAINT_ANALYTICS_DB_SAINT_MARTYRED_FACT_TABLE_NAME'))
+                saint_martyred_fact_table_name: str = getenv('SAINT_ANALYTICS_DB_SAINT_MARTYRED_FACT_TABLE_NAME')
+                saint_lake_table_name: str = getenv('SAINT_ANALYTICS_DB_SAINT_LAKE_TABLE_NAME')
+                loader_query: str = FACT_SAINT_MARTYRED_LOADER_QUERY.format(saint_martyred_fact_table_name=saint_martyred_fact_table_name, saint_lake_table_name=saint_lake_table_name)
+                formatted_martyred_query: str = MARTYRED_QUERY.format(saint_martyred_fact_table_name=saint_martyred_fact_table_name)
+                return '{loader_query} {formatted_martyred_query}'.format(loader_query=loader_query, formatted_martyred_query=formatted_martyred_query)
             case _:
                 return None
